ad76259eeae58883d57ff41c26e7d6000b390786
Burak Bilgili <burak.bilgili@icloud.com>
2025-03-07 10:28:43 +0300

    add new images for each theme

--- components/layout/image-carousel.tsx
+++ components/layout/image-carousel.tsx
@@ -25,6 +25,50 @@ import sp_c7 from '@/images/soft-pastel/c7.jpg';
 import sp_c8 from '@/images/soft-pastel/c8.jpg';
 import sp_c9 from '@/images/soft-pastel/c9.jpg';
 
+import lf_c0 from '@/images/lavender-fields/c0.jpg';
+import lf_c1 from '@/images/lavender-fields/c1.jpg';
+import lf_c2 from '@/images/lavender-fields/c2.jpg';
+import lf_c3 from '@/images/lavender-fields/c3.jpg';
+import lf_c4 from '@/images/lavender-fields/c4.jpg';
+import lf_c5 from '@/images/lavender-fields/c5.jpg';
+import lf_c6 from '@/images/lavender-fields/c6.jpg';
+import lf_c7 from '@/images/lavender-fields/c7.jpg';
+import lf_c8 from '@/images/lavender-fields/c8.jpg';
+import lf_c9 from '@/images/lavender-fields/c9.jpg';
+
+import mf_c0 from '@/images/mint-fresh/c0.jpg';
+import mf_c1 from '@/images/mint-fresh/c1.jpg';
+import mf_c2 from '@/images/mint-fresh/c2.jpg';
+import mf_c3 from '@/images/mint-fresh/c3.jpg';
+import mf_c4 from '@/images/mint-fresh/c4.jpg';
+import mf_c5 from '@/images/mint-fresh/c5.jpg';
+import mf_c6 from '@/images/mint-fresh/c6.jpg';
+import mf_c7 from '@/images/mint-fresh/c7.jpg';
+import mf_c8 from '@/images/mint-fresh/c8.jpg';
+import mf_c9 from '@/images/mint-fresh/c9.jpg';
+
+import ob_c0 from '@/images/ocean-breeze/c0.jpg';
+import ob_c1 from '@/images/ocean-breeze/c1.jpg';
+import ob_c2 from '@/images/ocean-breeze/c2.jpg';
+import ob_c3 from '@/images/ocean-breeze/c3.jpg';
+import ob_c4 from '@/images/ocean-breeze/c4.jpg';
+import ob_c5 from '@/images/ocean-breeze/c5.jpg';
+import ob_c6 from '@/images/ocean-breeze/c6.jpg';
+import ob_c7 from '@/images/ocean-breeze/c7.jpg';
+import ob_c8 from '@/images/ocean-breeze/c8.jpg';
+import ob_c9 from '@/images/ocean-breeze/c9.jpg';
+
+import sg_c0 from '@/images/sunset-glow/c0.jpg';
+import sg_c1 from '@/images/sunset-glow/c1.jpg';
+import sg_c2 from '@/images/sunset-glow/c2.jpg';
+import sg_c3 from '@/images/sunset-glow/c3.jpg';
+import sg_c4 from '@/images/sunset-glow/c4.jpg';
+import sg_c5 from '@/images/sunset-glow/c5.jpg';
+import sg_c6 from '@/images/sunset-glow/c6.jpg';
+import sg_c7 from '@/images/sunset-glow/c7.jpg';
+import sg_c8 from '@/images/sunset-glow/c8.jpg';
+import sg_c9 from '@/images/sunset-glow/c9.jpg';
+
 type PropTypes = { orientation: 'start' | 'end'; isBoxed?: boolean };
 
 const ImageCarousel = ({ orientation, isBoxed = true }: PropTypes) => {
@@ -37,13 +81,13 @@ const ImageCarousel = ({ orientation, isBoxed = true }: PropTypes) => {
         theme === 'theme-soft-pastel'
           ? sp_c0
           : theme === 'theme-lavender-fields'
-          ? '/placeholder.jpg'
+          ? lf_c0
           : theme === 'theme-mint-fresh'
-          ? '/placeholder.jpg'
+          ? mf_c0
           : theme === 'theme-ocean-breeze'
-          ? '/placeholder.jpg'
+          ? ob_c0
           : theme === 'theme-sunset-glow'
-          ? '/placeholder.jpg'
+          ? sg_c0
           : '/placeholder.jpg',
     },
     {
@@ -52,13 +96,13 @@ const ImageCarousel = ({ orientation, isBoxed = true }: PropTypes) => {
         theme === 'theme-soft-pastel'
           ? sp_c1
           : theme === 'theme-lavender-fields'
-          ? '/placeholder.jpg'
+          ? lf_c1
           : theme === 'theme-mint-fresh'
-          ? '/placeholder.jpg'
+          ? mf_c1
           : theme === 'theme-ocean-breeze'
-          ? '/placeholder.jpg'
+          ? ob_c1
           : theme === 'theme-sunset-glow'
-          ? '/placeholder.jpg'
+          ? sg_c1
           : '/placeholder.jpg',
     },
     {
@@ -67,13 +111,13 @@ const ImageCarousel = ({ orientation, isBoxed = true }: PropTypes) => {
         theme === 'theme-soft-pastel'
           ? sp_c2
           : theme === 'theme-lavender-fields'
-          ? '/placeholder.jpg'
+          ? lf_c2
           : theme === 'theme-mint-fresh'
-          ? '/placeholder.jpg'
+          ? mf_c2
           : theme === 'theme-ocean-breeze'
-          ? '/placeholder.jpg'
+          ? ob_c2
           : theme === 'theme-sunset-glow'
-          ? '/placeholder.jpg'
+          ? sg_c2
           : '/placeholder.jpg',
     },
     {
@@ -82,13 +126,13 @@ const ImageCarousel = ({ orientation, isBoxed = true }: PropTypes) => {
         theme === 'theme-soft-pastel'
           ? sp_c3
           : theme === 'theme-lavender-fields'
-          ? '/placeholder.jpg'
+          ? lf_c3
           : theme === 'theme-mint-fresh'
-          ? '/placeholder.jpg'
+          ? mf_c3
           : theme === 'theme-ocean-breeze'
-          ? '/placeholder.jpg'
+          ? ob_c3
           : theme === 'theme-sunset-glow'
-          ? '/placeholder.jpg'
+          ? sg_c3
           : '/placeholder.jpg',
     },
     {
@@ -97,13 +141,13 @@ const ImageCarousel = ({ orientation, isBoxed = true }: PropTypes) => {
         theme === 'theme-soft-pastel'
           ? sp_c4
           : theme === 'theme-lavender-fields'
-          ? '/placeholder.jpg'
+          ? lf_c4
           : theme === 'theme-mint-fresh'
-          ? '/placeholder.jpg'
+          ? mf_c4
           : theme === 'theme-ocean-breeze'
-          ? '/placeholder.jpg'
+          ? ob_c4
           : theme === 'theme-sunset-glow'
-          ? '/placeholder.jpg'
+          ? sg_c4
           : '/placeholder.jpg',
     },
     {
@@ -112,13 +156,13 @@ const ImageCarousel = ({ orientation, isBoxed = true }: PropTypes) => {
         theme === 'theme-soft-pastel'
           ? sp_c5
           : theme === 'theme-lavender-fields'
-          ? '/placeholder.jpg'
+          ? lf_c5
           : theme === 'theme-mint-fresh'
-          ? '/placeholder.jpg'
+          ? mf_c5
           : theme === 'theme-ocean-breeze'
-          ? '/placeholder.jpg'
+          ? ob_c5
           : theme === 'theme-sunset-glow'
-          ? '/placeholder.jpg'
+          ? sg_c5
           : '/placeholder.jpg',
     },
     {
@@ -127,13 +171,13 @@ const ImageCarousel = ({ orientation, isBoxed = true }: PropTypes) => {
         theme === 'theme-soft-pastel'
           ? sp_c6
           : theme === 'theme-lavender-fields'
-          ? '/placeholder.jpg'
+          ? lf_c6
           : theme === 'theme-mint-fresh'
-          ? '/placeholder.jpg'
+          ? mf_c6
           : theme === 'theme-ocean-breeze'
-          ? '/placeholder.jpg'
+          ? ob_c6
           : theme === 'theme-sunset-glow'
-          ? '/placeholder.jpg'
+          ? sg_c6
           : '/placeholder.jpg',
     },
     {
@@ -142,13 +186,13 @@ const ImageCarousel = ({ orientation, isBoxed = true }: PropTypes) => {
         theme === 'theme-soft-pastel'
           ? sp_c7
           : theme === 'theme-lavender-fields'
-          ? '/placeholder.jpg'
+          ? lf_c7
           : theme === 'theme-mint-fresh'
-          ? '/placeholder.jpg'
+          ? mf_c7
           : theme === 'theme-ocean-breeze'
-          ? '/placeholder.jpg'
+          ? ob_c7
           : theme === 'theme-sunset-glow'
-          ? '/placeholder.jpg'
+          ? sg_c7
           : '/placeholder.jpg',
     },
     {
@@ -157,13 +201,13 @@ const ImageCarousel = ({ orientation, isBoxed = true }: PropTypes) => {
         theme === 'theme-soft-pastel'
           ? sp_c8
           : theme === 'theme-lavender-fields'
-          ? '/placeholder.jpg'
+          ? lf_c8
           : theme === 'theme-mint-fresh'
-          ? '/placeholder.jpg'
+          ? mf_c8
           : theme === 'theme-ocean-breeze'
-          ? '/placeholder.jpg'
+          ? ob_c8
           : theme === 'theme-sunset-glow'
-          ? '/placeholder.jpg'
+          ? sg_c8
           : '/placeholder.jpg',
     },
     {
@@ -172,13 +216,13 @@ const ImageCarousel = ({ orientation, isBoxed = true }: PropTypes) => {
         theme === 'theme-soft-pastel'
           ? sp_c9
           : theme === 'theme-lavender-fields'
-          ? '/placeholder.jpg'
+          ? lf_c9
           : theme === 'theme-mint-fresh'
-          ? '/placeholder.jpg'
+          ? mf_c9
           : theme === 'theme-ocean-breeze'
-          ? '/placeholder.jpg'
+          ? ob_c9
           : theme === 'theme-sunset-glow'
-          ? '/placeholder.jpg'
+          ? sg_c9
           : '/placeholder.jpg',
     },
   ];
